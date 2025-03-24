bb8eabfbec74f08ef6731703ccdc89580030a1dc
Jakob Olsen <jolsen618@gmail.com>
2025-03-24 17:12:59 -0400

    Added writing to AWS table from serial data
    Fixed issue with data not being read until program termination

--- RaspberryPi/app.py
+++ RaspberryPi/app.py
@@ -1,5 +1,6 @@
 import boto3
 import serial
+import asyncio
 from boto3.dynamodb.conditions import Attr
 from botocore.exceptions import ClientError
 import uuid
@@ -8,12 +9,27 @@ dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
 table = dynamodb.Table("cc-testing")
 
 serialPort = serial.Serial(port="COM5", baudrate=9600, timeout=0)
-size = 12
-index = 100
+serialPort.write("Hello from Raspberry Pi!\n".encode())
+# wait for the arduino to send data
+def read_serial_data():
+    while True:
+        data = serialPort.readline().decode("utf-8").strip()
+        if data:
+            print("Data received from Arduino: ", data)
+            item = {"uuid": str(uuid.uuid4()), "message": data}
+            print ("Item: ", item)
+            table.put_item(Item=item)
+            print("Data saved to DynamoDB")
+            break
 
-while True:
-    data = serialPort.readline(13)
-    data = data.decode("utf-8")
+def read_table_data():
+    response = table.scan()
+    items = response["Items"]
+    print("Data read from DynamoDB: ", items)
 
-    if data:
-        print(data) # Currently has an issue that it will not print the data until program is closed (newline issue on the arduino side)
+read_serial_data()
+read_table_data()
+
+
+
+
